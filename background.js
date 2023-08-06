0a3f865a3ffdf12b8b8061713b748de832df3800
HarveyWho <haoxuanhong5@gmail.com>
2023-08-06 21:17:53 +0800

    Removed numbers in divs

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++){
         let div = document.createElement("div");
-        div.innerHTML=`${j+1}`
+        // div.innerHTML=`${j+1}`
         document.getElementById("container").appendChild(div);
     }
 }
